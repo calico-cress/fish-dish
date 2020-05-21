67c323dfe4d707a434120151c6e56053fba8df60
D--A--I <kanegadai@kanegadainoMacBook-Pro.local>
2020-05-21 18:04:49 +0900

    fix: changed to use standard input

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 import fs from 'fs';
-import handlErrors from './helper/handle-errors';
+import handleErrors from './helper/handle-errors';
 import parseArgs from './parse-args';
 import parseMail from './parse-mail';
 
 // 例外管理
-handlErrors();
+handleErrors();
 
-// +++ 引数はデコード対象のファイル or 件名に必須となる文字列 +++
-const argv = parseArgs(process.argv.slice(2));
+// +++ 引数は、解析対象を特定するための文字列（件名の一部） +++
+const subject = parseArgs(process.argv.slice(2)).subj;
 
 // 比較用の型
 interface Comparison {
@@ -17,14 +17,34 @@ interface Comparison {
   stat: fs.Stats;
 }
 
-// 更新日付の降順でソート
-const sorted = argv._.map(
-  (f: string): Comparison => ({ name: f, stat: fs.statSync(f) })
-).sort((p: Comparison, l: Comparison): number =>
-  p.stat.mtime <= l.stat.mtime ? 1 : -1
-);
+/**
+ * ファイル名（xN）を更新時刻の降順にする
+ * @param {string} line
+ * @returns {Comparison[]}
+ */
+function sort(line: string): Comparison[] {
+  const _map = (f: string): Comparison => ({ name: f, stat: fs.statSync(f) });
+  const _sort = (p: Comparison, l: Comparison): number =>
+    p.stat.mtime <= l.stat.mtime ? 1 : -1;
+  return line.split(' ').map(_map).sort(_sort); // 整形して返す
+}
 
-// 実行
-sorted.forEach((s: Comparison): void => {
-  parseMail(s.name, argv.subj);
-});
+/**
+ * メインのパース処理
+ * @param {Comparison[]} files
+ */
+const parse = (files: Comparison[]): void =>
+  files.forEach((f: Comparison): void => parseMail(f.name, subject));
+
+let input = ''; // 退避したチャンク
+process.stdin
+  .on('readable', (): void => {
+    let chunk: string | Buffer;
+    while ((chunk = process.stdin.read()) !== null) {
+      input += chunk.toString().trim();
+    }
+  })
+  .on('end', (): void => {
+    const files = sort(input);
+    parse(files);
+  });
